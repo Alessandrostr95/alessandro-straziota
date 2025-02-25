f6acb584681d46c3bc0a0ecc890e973ca2606b8d
Alessandrostr95 <alessandrostr95@gmail.com>
2025-02-25 23:47:31 +0100

    Quartz sync: Feb 25, 2025, 11:47 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Alessandro Straziota",
+    pageTitle: "Alessandro Straziota 🌱",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
